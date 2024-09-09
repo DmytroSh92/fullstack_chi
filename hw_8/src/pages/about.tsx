1c802c8673e7f17bdf8887029fc06c823ddfc460
Dmytro Shumov <dmytro.shumov@chisw.com>
2024-09-09 18:28:41 +0300

    hw_8

---
+++ hw_8/src/pages/about.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Welcome to the About page of our Next.js application!</p>
+        </div>
+    );
+};
+
+export default About;
